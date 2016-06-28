06ac024031f6651efd1ce035bc621bc90ba8140e
Sneha Sonkere <snehassonkere96@gmail.com>
2016-06-28 13:36:46 +0530

    Create Logs model

--- googleSheets/sheets_io/models.py
+++ googleSheets/sheets_io/models.py
@@ -3,3 +3,17 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Logs(models.Model):
+    from_sheet = models.CharField(max_length=100)
+    to_sheet = models.CharField(max_length=100)
+    from_tab = models.CharField(max_length=100)
+    to_tab = models.CharField(max_length=100)
+    updated_at = models.DateTimeField(auto_now=True)
+    updated_row = models.CharField(max_length=10)
+    updated_col = models.CharField(max_length=10)
+    updated_range = models.CharField(max_length=100)
+
+    def __str__(self):
+        return "{}!{} -> {}!{}: {}".format(self.from_sheet,self.from_tab,self.to_sheet,self.to_tab,str(self.updated_at))
