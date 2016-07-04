b67e1ec4649f2320a12943a0366635b52f060157
navitasinghal <navitasinghal77@gmail.com>
2016-07-04 11:52:32 +0530

    Add admin static files

--- googleSheets/sheets_io/schedule.py
+++ googleSheets/sheets_io/schedule.py
@@ -1,8 +1,11 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
-
+import httplib2
+import requests
 def update_sheet():
+    http=httplib2.HTTP()
+    _, data = http.request("")
     print "updated"
 
 schedular = BlockingScheduler()
-schedular.add_job(update_sheet, 'interval', seconds=3)
+schedular.add_job(update_sheet, 'interval', hours=12)
 schedular.start()
