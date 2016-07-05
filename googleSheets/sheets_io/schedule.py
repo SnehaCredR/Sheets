2f1fbf2a01b6c98a84a8ce0f630c5b22d9f7740a
navitasinghal <navitasinghal77@gmail.com>
2016-07-05 14:50:19 +0530

    change schedule.py

--- googleSheets/sheets_io/schedule.py
+++ googleSheets/sheets_io/schedule.py
@@ -3,9 +3,10 @@ import httplib2
 import requests
 def update_sheet():
     http=httplib2.HTTP()
-    _, data = http.request("")
+    _, data = http.request("http://shelog.credr.io/sheets/index/")
     print "updated"
 
+
 schedular = BlockingScheduler()
-schedular.add_job(update_sheet, 'interval', hours=12)
+schedular.add_job(update_sheet, 'interval', seconds=60)
 schedular.start()
